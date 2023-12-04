43a00cc44ec9ba47fcedeab2c4102e9ea5a3d195
Faza Denandra <112379648+fzdeltaa@users.noreply.github.com>
2023-12-04 11:05:57 +0700

    auth jadi
    update credentials kelupaan hehe

--- server.js
+++ server.js
@@ -1,8 +1,37 @@
 const express = require('express');
+const admin = require('firebase-admin');
+
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
+const serviceAccount = require("./credentials.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+// Middleware
+// TODO: Pisahkan middleware ke folder lain
+const authenticateToken = (req, res, next) => {
+  const idToken = req.header('Authorization');
+
+  if (!idToken) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  admin.auth().verifyIdToken(idToken)
+    .then((decodedToken) => {
+      req.user = decodedToken;
+      next();
+    })
+    .catch((error) => {
+      console.error('Error verifying token:', error);
+      return res.status(401).json({ error: 'Unauthorized' });
+    });
+};
+
+// TODO: Pisahkan route
+app.get('/', authenticateToken, (req, res) => {
   const name = process.env.NAME || 'World';
   res.send(`Hello ${name}!`);
 });
@@ -13,8 +42,7 @@ app.listen(port, () => {
 
 /*
 TODO:
-1. Folder routes terpisah
+1. mvc
 2. Koneksi db
-3. private endpoint with token
-4. ?? 
+4. Logout ??????????????????
 */
