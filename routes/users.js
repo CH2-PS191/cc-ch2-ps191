a04212e35878866c9bbf4e2a6ce9ca118c870066
Faza Denandra <112379648+fzdeltaa@users.noreply.github.com>
2023-12-18 18:56:34 +0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -114,23 +114,18 @@ router.get('/sebaya', authenticateToken, (req, res) => {
     });
 });
 
-router.post('/conversationid', authenticateToken, (req, res) => {
+router.post('/conversationclaims', authenticateToken, (req, res) => {
   admin.auth()
   .setCustomUserClaims(req.user.uid, { conversationId: req.body.conversationId })
   .then(
     admin.auth()
       .getUser(req.user.uid)
       .then((userRecord) => {
-        res.status(200).send(userRecord.toJSON());
-      }).then(
-        admin.auth().getUser(req.user.uid).then((userdata) => {
-          console.log(userdata.customClaims.conversationId);
-        }
-        )
-      )
+        res.status(200).json({ conversationId: userRecord.customClaims.conversationId });
+      })
   ).catch((error) => {
     console.log(error);
-    res.status(500).send(error);
+    res.status(500).json({ error });
   });
 });
 
